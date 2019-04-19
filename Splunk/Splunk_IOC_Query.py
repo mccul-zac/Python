df3c6d39cf304250421c94604c2f0e9061f7f8a7
Zulu-Onion <40836609+Zulu-Onion@users.noreply.github.com>
2019-04-19 10:44:06 -0400

    Update Splunk_IOC_Query.py

--- Splunk/Splunk_IOC_Query.py
+++ Splunk/Splunk_IOC_Query.py
@@ -1,5 +1,6 @@
 # Take IOC list (input OR file) and spit out a Splunk query
 # Should include option to apply additional fields
+# Edit print as necessary (include/exclude splunk fields)
 
 with open(r"ioc.txt") as file:                        # requires full path to file
     ph = ' OR '.join(line.rstrip() for line in file)
