bb7728166828d076b2e785c14671681d061dc7c8
Zulu-Onion <40836609+Zulu-Onion@users.noreply.github.com>
2019-04-30 08:41:35 -0400

    Create extract_dst_ips.py

---
+++ pdf/extract_dst_ips.py
@@ -0,0 +1,26 @@
+# Dom E. & Zach M.
+# 4/26/2019
+
+import re, sys
+
+pdfFileObjx = open('pdfStrings.txt', 'rb').read()
+pdfFileObj = pdfFileObjx.decode('utf-16')
+
+newList = []
+finalDstIPList = []
+ipList = []
+
+ipList = re.findall('ip.dst=(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])', pdfFileObj)
+
+for i in ipList:
+    if i not in newList:
+        newList.append(i)
+
+for item in newList:
+    q = item.strip('ip.dst=')
+    finalDstIPList.append(q)
+
+for item in finalDstIPList:
+    print(item)
+
+print('\n\nTotal External IP Addresses:', len(ipList), '\n\nTotal unique IP Addresses:', len(finalDstIPList))
