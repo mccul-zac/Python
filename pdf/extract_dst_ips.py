4ad8e119b4f03b84918aa02cb9c63aa2111e7f85
Zulu-Onion <40836609+Zulu-Onion@users.noreply.github.com>
2019-04-30 15:02:59 -0400

    Update extract_dst_ips.py

--- pdf/extract_dst_ips.py
+++ pdf/extract_dst_ips.py
@@ -3,26 +3,32 @@
 # run strings on PDF file and search txt for the following:
 
 
-import re, sys
+import re
 
-pdfFileObjx = open('pdfStrings.txt', 'rb').read()
-pdfFileObj = pdfFileObjx.decode('utf-16')
+#fileName = input('File Path: ')
+# OR
+fileName = 'pdfStrings.txt'
+
 
 newList = []
 finalDstIPList = []
-ipList = []
 
-ipList = re.findall('ip.dst=(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])', pdfFileObj)
+regexIP = re.compile('ip.dst=(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])')
+ipList = regexIP.findall(open(fileName, 'r').read())
 
 for i in ipList:
-    if i not in newList:
-        newList.append(i)
+  if i not in newList:
+    newList.append(i)
 
 for item in newList:
-    q = item.strip('ip.dst=')
-    finalDstIPList.append(q)
+  q = item.strip('ip.dst=')
+  finalDstIPList.append(q)
 
+
+print('Total External IP Addresses:', len(ipList), '\n\nTotal unique IP Addresses:', len(finalDstIPList), '\n')
+
+print('IPs to search:')
 for item in finalDstIPList:
     print(item)
 
-print('\n\nTotal External IP Addresses:', len(ipList), '\n\nTotal unique IP Addresses:', len(finalDstIPList))
+print('')
