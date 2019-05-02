ee963219f96560e7f08adf43bddf2790cbec4f39
Zulu-Onion <40836609+Zulu-Onion@users.noreply.github.com>
2019-05-02 15:40:10 -0400

    Update curlTLD.py

--- Functions/curlTLD.py
+++ Functions/curlTLD.py
@@ -1,12 +1,13 @@
 # Not really curl (Invoke-WebRequest)
-# Need to remove title line (currently title line is separated into 11 lines...)
-# With open...phase does not work (maybe subprocess is still open?)
+# Thanks to Ben for killing the powershell
 
-import subprocess, os
+import subprocess, os, time
 
 def curlTLD():
     if os.path.isfile('./tld.txt') == False:
-        subprocess.Popen(['powershell', '$tld = curl http://data.iana.org/TLD/tlds-alpha-by-domain.txt | Select-Object -Expand Content;$tld.split() | Out-File -Encoding ASCII tld.txt'])
+        pro = subprocess.Popen(['powershell', '$tld = curl http://data.iana.org/TLD/tlds-alpha-by-domain.txt | Select-Object -Expand Content;$tld.split() | Out-File -Encoding ASCII tld.txt'])
+        time.sleep(2.0)
+        pro.kill()
         with open('tld.txt', 'r') as fin:
             data = fin.read().splitlines(True)
             with open('tld.txt', 'w') as fout:
