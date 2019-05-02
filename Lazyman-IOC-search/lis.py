fa035572a50cbc0f0053a45a291970bbbc46f4d1
Zulu-Onion <40836609+Zulu-Onion@users.noreply.github.com>
2019-05-02 10:19:26 -0400

    Update lis.py

--- Lazyman-IOC-search/lis.py
+++ Lazyman-IOC-search/lis.py
@@ -7,9 +7,17 @@
 
 import os,re
 
-
 fileName = 'ioc.txt'
 #fileName = input('File Path: ')
+
+domainList = []
+finalEmailList = []
+finalDomainList = []
+MD5List = []
+SHA1List = []
+SHA256List = []
+fullList = []
+remainingList = []
 
 '''
 def curlTLD():
@@ -28,132 +36,9 @@ def removeNewline(myList):
   [betterList.append(i.strip()) for i in myList]
   return betterList
 
-
 def split_file(filename):
   done = list(filename.split('\n'))
   return done
-
-
-
-
-with open(fileName, 'r') as fanged:
-    content = fanged.read()
-refanged = re.sub('\[\.\]', r'.', content)
-blah = open('live_ioc.txt', 'w')
-blah.write(refanged)
-blah.close()
-
-
-refangedFile = open('live_ioc.txt', 'r')
-#print(refangedFile.read())
-prime = refangedFile.read()
-
-
-
-
-########################################
-
-regexIP = re.compile('(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])(?:$|\n)')
-ipList = regexIP.findall(prime)
-finalIPList = removeNewline(ipList)
-
-
-########################################
-
-
-
-
-filename = open('tld.txt').read()
-qqq = split_file(filename)
-
-domainList = []
-#def domain_regex():
-for var in qqq: ###my TLD list loop
-  regexDomain = re.compile(f'(?:.+\.{var}(?:$|\n))', re.IGNORECASE) ###regex for each tld 
-  if len(regexDomain.findall(prime)) > 0:
-    domainList.extend(regexDomain.findall(prime))
-
-
-
-
-
-bigDomainList = removeNewline(domainList)
-
-
-finalEmailList = []
-finalDomainList = []
-
-for r in bigDomainList:
-  if '@' in r:
-    finalEmailList.append(r)
-  else:
-    finalDomainList.append(r)
-
-
-
-########################################
-
-MD5List = []
-SHA1List = []
-SHA256List = []
-
-regexHash = re.compile('[a-f0-9]{32,64}(?:$|\n)', re.IGNORECASE)
-hashList = regexHash.findall(prime)
-
-finalHashList = removeNewline(hashList)
-
-
-
-for hashVal in finalHashList:
-  if len(hashVal) == 32:
-    MD5List.append(hashVal) 
-  elif len(hashVal) == 40:
-    SHA1List.append(hashVal)
-  elif len(hashVal) == 64:
-    SHA256List.append(hashVal)
-  else:
-    pass
-
-fullList = []
-fullList.extend(hashList)
-fullList.extend(domainList)
-fullList.extend(ipList)
-
-
-
-remainingList = []
-newFile = r'live_ioc.txt'
-
-with open(newFile, 'r') as thisguy:
-    content2 = thisguy.readlines()
-    for line in content2:
-      if line not in fullList:
-        remainingList.append(line)
-
-finalRemainingList = removeNewline(remainingList)
-
-
-
-'''
-print(finalIPList)
-print('')
-print(finalHashList)
-print('')
-print(finalDomainList)
-print('')
-print(finalEmailList)
-print('')
-print(finalRemainingList)
-print('')
-'''
-
-####################################
-'''
-def ip_format_search(iplist):
-  for line in iplist:
-    ph = ' || ip.all = ' + line
-    return (f'ip.all = {ph}')
-'''
 
 def ip_format_search(iocList):
   ph = ' || ip.all = '.join(line.rstrip() for line in iocList)
@@ -177,9 +62,57 @@ def file_format_search(iocList):
   ph = ' || filename = \"'.join(line.rstrip()+"\"" for line in iocList)
   print(f'filename = \"{ph}')
 
+with open(fileName, 'r') as fanged:
+    content = fanged.read()
+refanged = re.sub('\[\.\]', r'.', content)
+blah = open('live_ioc.txt', 'w')
+blah.write(refanged)
+blah.close()
 
+refangedFile = open('live_ioc.txt', 'r')
+prime = refangedFile.read()
 
+regexIP = re.compile('(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])(?:$|\n)')
+ipList = regexIP.findall(prime)
+finalIPList = removeNewline(ipList)
 
+filename = open('tld.txt').read()
+qqq = split_file(filename)
+for var in qqq: ###my TLD list loop
+  regexDomain = re.compile(f'(?:.+\.{var}(?:$|\n))', re.IGNORECASE) ###regex for each tld 
+  if len(regexDomain.findall(prime)) > 0:
+    domainList.extend(regexDomain.findall(prime))
+bigDomainList = removeNewline(domainList)
+for r in bigDomainList:
+  if '@' in r:
+    finalEmailList.append(r)
+  else:
+    finalDomainList.append(r)
+
+regexHash = re.compile('[a-f0-9]{32,64}(?:$|\n)', re.IGNORECASE)
+hashList = regexHash.findall(prime)
+finalHashList = removeNewline(hashList)
+for hashVal in finalHashList:
+  if len(hashVal) == 32:
+    MD5List.append(hashVal) 
+  elif len(hashVal) == 40:
+    SHA1List.append(hashVal)
+  elif len(hashVal) == 64:
+    SHA256List.append(hashVal)
+  else:
+    pass
+
+fullList.extend(hashList)
+fullList.extend(domainList)
+fullList.extend(ipList)
+
+newFile = r'live_ioc.txt'
+with open(newFile, 'r') as thisguy:
+    content2 = thisguy.readlines()
+    for line in content2:
+      if line not in fullList:
+        remainingList.append(line)
+finalRemainingList = removeNewline(remainingList)
 
 ip_format_search(finalIPList)
 print('')
@@ -187,4 +120,6 @@ url_format_search(finalDomainList)
 print('')
 hash_format_search(finalHashList)
 print('')
+#email_format_search(finalEmailList)
+#print('')
 file_format_search(finalRemainingList)
