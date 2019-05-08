f72d5cac13c92836c4b69fc8cace167f4502cba3
Zulu-Onion <40836609+Zulu-Onion@users.noreply.github.com>
2019-05-08 09:51:11 -0400

    Update lis.py
    Changes made to output

--- Lazyman-IOC-search/lis.py
+++ Lazyman-IOC-search/lis.py
@@ -1,6 +1,6 @@
 '''
-# Authors: Ben G. & Zach M.
-# 4/20/2019
+# Authors: Ben G.& Zach M.
+# 5/3/2019
 '''
 
 import os,re,subprocess,time,zipfile
@@ -18,47 +18,56 @@ def split_file(filename):
 
 def ip_format_search(iocList, searchSyntax):
   # outputs Netwitness syntax for IP addresses and appends to syntax file
-  ph = ' || ip.all = '.join(line.rstrip() for line in iocList)
-  print(f'ip.all = {ph}')
-  #with open(searchSyntax, 'a') as searchSyntax:
-  searchSyntax.write("\n\n"f'ip.all = {ph}')
+    ph = ' || ip.all = '.join(line.rstrip() for line in iocList)
+    if ph != '':
+        print(f'ip.all = {ph}')
+        searchSyntax.write("\n\n"f'ip.all = {ph} || ')
+    else:
+        return
 
 def url_format_search(iocList, searchSyntax):
   # outputs Netwitness syntax for domain and appends to syntax file
-  ph = ' || domain.dst = \"'.join(line.rstrip()+"\"" for line in iocList)
-  print(f'domain.dst = \"{ph}')
-  #with open(searchSyntax, 'a') as searchSyntax:
-  searchSyntax.write("\n\n"f'domain.dst = \"{ph}')
+    ph = ' || domain.dst = \"'.join(line.rstrip()+"\"" for line in iocList)
+    if ph != '':
+        print(f'domain.dst = \"{ph}')
+        searchSyntax.write(f'domain.dst = \"{ph} || ')
+    else:
+        return
 
 def hash_format_search(iocList, searchSyntax):
   # outputs Netwitness syntax for hashes and appends to syntax file
-  ph = ' || checksum = \"'.join(line.rstrip()+"\"" for line in iocList)
-  print(f'checksum = \"{ph}')
-  #with open(searchSyntax, 'a') as searchSyntax:
-  searchSyntax.write("\n\n"f'checksum = \"{ph}')
+    ph = ' || checksum = \"'.join(line.rstrip()+"\"" for line in iocList)
+    if ph != '':
+        print(f'checksum = \"{ph}')
+        searchSyntax.write(f'checksum = \"{ph} || ')
+    else:
+        return
 
 def email_format_search(iocList, searchSyntax):
   # outputs Netwitness syntax for email addresses and appends to syntax file
-  ph = ' || email.all = \"'.join(line.rstrip()+"\"" for line in iocList)
-  print(f'email.all = \"{ph}')
-  #with open(searchSyntax, 'a') as searchSyntax:
-  searchSyntax.write("\n\n"f'email.all = \"{ph}')
+    ph = ' || email.all = \"'.join(line.rstrip()+"\"" for line in iocList)
+    if ph != '':
+        print(f'email.all = \"{ph}')
+        searchSyntax.write(f'email.all = \"{ph} || ')
+    else:
+        return
 
 def file_format_search(iocList, searchSyntax):
   # outputs Netwitness syntax for files/extra "stuff" and appends to syntax file
-  ph = ' || filename = \"'.join(line.rstrip()+"\"" for line in iocList)
-  print(f'filename = \"{ph}')
-  #with open(searchSyntax, 'a') as searchSyntax:
-  searchSyntax.write("\n\n"f'filename = \"{ph}')
+    ph = ' || filename = \"'.join(line.rstrip()+"\"" for line in iocList)
+    if ph != '':
+        print(f'filename = \"{ph}')
+        searchSyntax.write(f'filename = \"{ph}')
+    else:
+        return
 
 def SplunkSearch(searchSyntax):
   # outputs Splunk syntax for all IOCs and appends to syntax file
     newFile = r'live_ioc.txt'
     with open(newFile, 'r') as liveIoc:
-        ph = ' OR '.join(line.rstrip() for line in liveIoc)
+        ph = ' OR \"'.join(line.rstrip()+"\"" for line in liveIoc)
         print(f'index = * {ph}')
-        #with open(searchSyntax, 'a') as searchSyntax:
-        searchSyntax.write("\n\n"f'index = * {ph}')
+        searchSyntax.write("\n\n"f'index=* ("{ph}) earliest=-90d')
 
 def curlTLD():
   '''
@@ -70,30 +79,28 @@ def curlTLD():
         time.sleep(4.0)
         pro.kill()
         if os.path.isfile('./tld.txt') == True:
-            print("file exists now...")
             with open('tld.txt', 'r') as fin:
                 data = fin.read().splitlines(True)
                 with open('tld.txt', 'w') as fout:
                     fout.writelines(data[12:])
 
+
 if __name__ == '__main__':
     '''
     Input: ioc.txt
-    Processing: 
+    Processing:
       Opens the IOC file, reads through each line and re-fangs the IOCs if they have been de-fanged
       Uses regex to look for IP addresses and add them to finalIPList
       Call CurlTLD function to look for the TLD file or create a new one, then look for domains with those TLDs
       Use regex to look for hash values of types MD5, SHA1, or SHA256 and add matches to respective lists
       Add previous findings to fullList
-      Create file IOCsearchSyntax.txt 
-    Output: 
-      Send output of previously created lists to respective print functions and print RSA Netwitness formatted searches 
+      Create file IOCsearchSyntax.txt
+    Output:
+      Send output of previously created lists to respective print functions and print RSA Netwitness formatted searches
       and Splunk searches, in addition to appending searches to IOCsearchSyntax.txt
       Finally, zip IOCsearchSyntax.txt
     '''
     fileName = 'ioc.txt'
-    # fileName = input('File Path: ')
-
     domainList = []
     finalEmailList = set()
     finalDomainList = []
@@ -116,6 +123,7 @@ if __name__ == '__main__':
     regexIP = re.compile('(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])(?:$|\n)')
     ipList = regexIP.findall(prime)
     finalIPList = removeNewline(ipList)
+    refangedFile.close()
 
     curlTLD()
     filename = open('tld.txt').read()
@@ -149,11 +157,12 @@ if __name__ == '__main__':
     fullList.extend(ipList)
 
     newFile = r'live_ioc.txt'
-    with open(newFile, 'r') as iocOut:
-        content2 = iocOut.readlines()
-        for line in content2:
-          if line not in fullList:
+    iocOut = open(newFile, 'r')
+    content2 = iocOut.readlines()
+    for line in content2:
+        if line not in fullList:
             remainingList.append(line)
+    iocOut.close()
     finalRemainingList = removeNewline(remainingList)
 
     searchSyntax = 'IOCsearchSyntax.txt'
@@ -173,5 +182,16 @@ if __name__ == '__main__':
       print('*** Splunk Search ***\n')
       SiemSearch.write("\n\n" + "*"*10 + "   Splunk Search   " + "*"*10+ "\n")
       SplunkSearch(SiemSearch)
-
+    # Zip the file for email purpose
     zipfile.ZipFile('IOCsearchSyntax.zip', mode='w').write(searchSyntax)
+
+    # Delete the files created earlier except for the search syntax files
+    if os.path.exists("live_ioc.txt"):
+        os.remove("live_ioc.txt")
+    else:
+        print("The file does not exist")
+
+    if os.path.exists("tld.txt"):
+        os.remove("tld.txt")
+    else:
+        print("The file does not exist")
