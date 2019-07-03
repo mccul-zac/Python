503428b944c6bbdcfabc8ce94a4b13c2bbc35582
mccul-zac <50666315+mccul-zac@users.noreply.github.com>
2019-07-03 12:08:47 -0400

    Update blind_chess.py

--- other/blind_chess/blind_chess.py
+++ other/blind_chess/blind_chess.py
@@ -3,84 +3,86 @@ two_kings_on_board = True
 board = {
     "a1": ['WRa'],
     "a2": ['WPa'],
-    "a3": [''],
-    "a4": [''],
-    "a5": [''],
-    "a6": [''],
+    "a3": ['   '],
+    "a4": ['   '],
+    "a5": ['   '],
+    "a6": ['   '],
     "a7": ['BPa'],
     "a8": ['BRa'],
     "b1": ['WNa'],
     "b2": ['WPb'],
-    "b3": [''],
-    "b4": [''],
-    "b5": [''],
-    "b6": [''],
+    "b3": ['   '],
+    "b4": ['   '],
+    "b5": ['   '],
+    "b6": ['   '],
     "b7": ['BPb'],
     "b8": ['BNa'],
     "c1": ['WBa'],
     "c2": ['WPc'],
-    "c3": [''],
-    "c4": [''],
-    "c5": [''],
-    "c6": [''],
+    "c3": ['   '],
+    "c4": ['   '],
+    "c5": ['   '],
+    "c6": ['   '],
     "c7": ['BPc'],
     "c8": ['BBa'],
-    "d1": ['WK'],
+    "d1": ['WK-'],
     "d2": ['WPd'],
-    "d3": [''],
-    "d4": [''],
-    "d5": [''],
-    "d6": [''],
+    "d3": ['   '],
+    "d4": ['   '],
+    "d5": ['   '],
+    "d6": ['   '],
     "d7": ['BPd'],
-    "d8": ['BK'],
-    "e1": ['WQ'],
+    "d8": ['BK-'],
+    "e1": ['WQ-'],
     "e2": ['WPe'],
-    "e3": [''],
-    "e4": [''],
-    "e5": [''],
-    "e6": [''],
+    "e3": ['   '],
+    "e4": ['   '],
+    "e5": ['   '],
+    "e6": ['   '],
     "e7": ['BPe'],
-    "e8": ['BQ'],
+    "e8": ['BQ-'],
     "f1": ['WBb'],
     "f2": ['WPf'],
-    "f3": [''],
-    "f4": [''],
-    "f5": [''],
-    "f6": [''],
+    "f3": ['   '],
+    "f4": ['   '],
+    "f5": ['   '],
+    "f6": ['   '],
     "f7": ['BPf'],
     "f8": ['BBb'],
     "g1": ['WNb'],
     "g2": ['WPg'],
-    "g3": [''],
-    "g4": [''],
-    "g5": [''],
-    "g6": [''],
+    "g3": ['   '],
+    "g4": ['   '],
+    "g5": ['   '],
+    "g6": ['   '],
     "g7": ['BPg'],
     "g8": ['BNb'],
     "h1": ['WRb'],
     "h2": ['WPh'],
-    "h3": [''],
-    "h4": [''],
-    "h5": [''],
-    "h6": [''],
+    "h3": ['   '],
+    "h4": ['   '],
+    "h5": ['   '],
+    "h6": ['   '],
     "h7": ['BPh'],
     "h8": ['BRb']
 }
 
 
 def current_board():
-    print(board['a8'], board['b8'], board['c8'], board['d8'], board['e8'], board['f8'], board['g8'], board['h8'])
-    print(board['a7'], board['b7'], board['c7'], board['d7'], board['e7'], board['f7'], board['g7'], board['h7'])
-    print(board['a6'], board['b6'], board['c6'], board['d6'], board['e6'], board['f6'], board['g6'], board['h6'])
-    print(board['a5'], board['b5'], board['c5'], board['d5'], board['e5'], board['f5'], board['g5'], board['h5'])
-    print(board['a4'], board['b4'], board['c4'], board['d4'], board['e4'], board['f4'], board['g4'], board['h4'])
-    print(board['a3'], board['b3'], board['c3'], board['d3'], board['e3'], board['f3'], board['g3'], board['h3'])
-    print(board['a2'], board['b2'], board['c2'], board['d2'], board['e2'], board['f2'], board['g2'], board['h2'])
-    print(board['a1'], board['b1'], board['c1'], board['d1'], board['e1'], board['f1'], board['g1'], board['h1'])
+    print('          [ A-8 ][ B-8 ][ C-8 ][ D-8 ][ E-8 ][ F-8 ][ G-8 ][ H-8 ]')
+    print('             -      -      -      -      -      -      -      -   ')
+    print('[ A-8 ] - ' + str(board['a8']) + "" + str(board['b8']) + "" + str(board['c8']) + "" + str(board['d8']) + "" + str(board['e8']) + "" + str(board['f8']) + "" + str(board['g8']) + "" + str(board['h8']))
+    print('[ A-7 ] - ' + str(board['a7']) + "" + str(board['b7']) + "" + str(board['c7']) + "" + str(board['d7']) + "" + str(board['e7']) + "" + str(board['f7']) + "" + str(board['g7']) + "" + str(board['h7']))
+    print('[ A-6 ] - ' + str(board['a6']) + "" + str(board['b6']) + "" + str(board['c6']) + "" + str(board['d6']) + "" + str(board['e6']) + "" + str(board['f6']) + "" + str(board['g6']) + "" + str(board['h6']))
+    print('[ A-5 ] - ' + str(board['a5']) + "" + str(board['b5']) + "" + str(board['c5']) + "" + str(board['d5']) + "" + str(board['e5']) + "" + str(board['f5']) + "" + str(board['g5']) + "" + str(board['h5']))
+    print('[ A-4 ] - ' + str(board['a4']) + "" + str(board['b4']) + "" + str(board['c4']) + "" + str(board['d4']) + "" + str(board['e4']) + "" + str(board['f4']) + "" + str(board['g4']) + "" + str(board['h4']))
+    print('[ A-3 ] - ' + str(board['a3']) + "" + str(board['b3']) + "" + str(board['c3']) + "" + str(board['d3']) + "" + str(board['e3']) + "" + str(board['f3']) + "" + str(board['g3']) + "" + str(board['h3']))
+    print('[ A-2 ] - ' + str(board['a2']) + "" + str(board['b2']) + "" + str(board['c2']) + "" + str(board['d2']) + "" + str(board['e2']) + "" + str(board['f2']) + "" + str(board['g2']) + "" + str(board['h2']))
+    print('[ A-1 ] - ' + str(board['a1']) + "" + str(board['b1']) + "" + str(board['c1']) + "" + str(board['d1']) + "" + str(board['e1']) + "" + str(board['f1']) + "" + str(board['g1']) + "" + str(board['h1']))
 
 
 while two_kings_on_board:
-    if ((['WK'] in board.values()) & (['BK'] in board.values())):
+    if ((['WK-'] in board.values()) & (['BK-'] in board.values())):
         # moves are defined position to position (i.e.,user input = 'a2 a4')
         print('')
         print('')
@@ -89,7 +91,7 @@ while two_kings_on_board:
         swap_keys = move_input.split(' ')
         # Need to figure out how to define checkmate without actually taking the king's position
         #Move to unoccupied space
-        if ((board[swap_keys[0]] != ['']) & (board[swap_keys[1]] == [''])):
+        if ((board[swap_keys[0]] != ['   ']) & (board[swap_keys[1]] == ['   '])):
             print('')
             print('')
             print('Before: ' + str(board[swap_keys[0]]) + str(board[swap_keys[1]]))
@@ -104,14 +106,14 @@ while two_kings_on_board:
             print('')
             current_board()
         #Moving to occupied space (take a piece off the board)
-        elif ((board[swap_keys[0]] != ['']) & (board[swap_keys[1]] != [''])):
+        elif ((board[swap_keys[0]] != ['   ']) & (board[swap_keys[1]] != ['   '])):
             #If spot1 and spot2 have pieces
             print('')
             print('')
             print('Before: ' + str(board[swap_keys[0]]) + str(board[swap_keys[1]]))
             x = board[swap_keys[0]]
             y = board[swap_keys[1]]
-            board[swap_keys[0]] = ['']
+            board[swap_keys[0]] = ['   ']
             board[swap_keys[1]] = x
             print('')
             print('')
@@ -133,4 +135,16 @@ while two_kings_on_board:
         two_kings_on_board = False
         print('')
         print('')
-        print('Checkmate!')
+        print('''
+
+        _________ .__                   __      _____          __        ._._._.
+        \_   ___ \|  |__   ____   ____ |  | __ /     \ _____ _/  |_  ____| | | |
+        /    \  \/|  |  \_/ __ \_/ ___\|  |/ //  \ /  \\__  \\   __\/ __ \ | | |
+        \     \___|   Y  \  ___/\  \___|    </    Y    \/ __ \|  | \  ___/\|\|\|
+         \______  /___|  /\___  >\___  >__|_ \____|__  (____  /__|  \___  >_____
+
+        ''')
+
+
+
+
